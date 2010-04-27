f61ac71697af222f19628e5e7bc80b3c5de2ffe1
J Brandon Keith <jbrkeith@gmail.com>
2010-04-27 19:30:44 +0000

    adding updater to matter package and uploading to server and registering on pypi...now it is easy installable

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ import fix_setuptools_chmod
 # define distribution
 setup(
         name = "matter",
-        version = "1.0",
+        version = "0.8",
         #namespace_packages = ['matter'],
         packages = find_packages(exclude=['tests']),
         test_suite = 'tests',
@@ -19,24 +19,26 @@ setup(
             'PyCifRW',
         ],
         dependency_links = [
-            'http://www.diffpy.org/packages/',
+            'http://dev.danse.us/packages/',
         ],
 
         author = 'J. Brandon Keith',
         author_email = 'jbrkeith@gmail.edu',
-        description = "matter classes and parsers for structure formats.",
+        description = "matter classes and parsers for structure formats",
         license = 'BSD',
-        keywords = "matter material",
+        keywords = "matter material crystal structure symmetry",
+        url = "http://docs.danse.us/inelastic/vsat/",
+        download_url = 'http://dev.danse.us/packages/',
         classifiers = [
             # List of possible values at
             # http://pypi.python.org/pypi?:action=list_classifiers
-            'Development Status :: 5 - Production/Stable',
+            'Development Status :: 4 - Beta',
             'Environment :: Console',
             'Intended Audience :: Science/Research',
             'Operating System :: MacOS',
             'Operating System :: Microsoft :: Windows',
             'Operating System :: POSIX',
-            'Programming Language :: Python :: 2.5',
+            'Programming Language :: Python :: 2.6',
             'Topic :: Scientific/Engineering :: Physics',
         ],
 )
