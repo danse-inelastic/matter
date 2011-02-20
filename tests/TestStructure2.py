356102430de7a7ff412ab1de8c5e602a564748fa
J Brandon Keith <jbrkeith@gmail.com>
2011-02-20 04:33:08 +0000

    * adding a lead cif file for testing * a couple new tests

--- tests/TestStructure2.py
+++ tests/TestStructure2.py
@@ -35,11 +35,22 @@ class TestStructure(unittest.TestCase):
         self.stru.read(xyzfile)
         #print self.stru5
         
+        file = os.path.join(testdata_dir, 'Pb.cif')
+        self.stru2 = Structure()
+        self.stru2.read(file)
+        #print self.stru2
+        
 #        xyzfile = os.path.join(testdata_dir, 'KC24PosAndCharges.xyz')
 #        self.stru2 = Structure()
 #        self.stru2.read(xyzfile)
 
         self.places = 12  
+        
+    def testRS(self):
+        """reciprocal space tests"""
+        print self.stru2.lattice
+        print self.stru2.lattice.recbase
+        print self.stru2.lattice.recbase2pi  
         
     def assertListAlmostEqual(self, l1, l2, places=None):
         """wrapper for list comparison"""
