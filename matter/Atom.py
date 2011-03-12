a55dd2091d1d6516c29b1d5e9999d07bcb66f738
J Brandon Keith <jbrkeith@gmail.com>
2011-03-12 03:22:57 +0000

    * fixing a major bug in the Atom constructor--somehow it was setting all xyz coordinates to 0...

--- matter/Atom.py
+++ matter/Atom.py
@@ -124,7 +124,7 @@ class Atom(object):
                  occupancy=1.0, charge=0.0, anisotropy=None, U=None, Uisoequiv=None, lattice=None):
         object.__init__(self)
         # declare non-singleton data members
-        self.xyz = numpy.zeros(3, dtype=float)
+        self.xyz = xyz
         self.label = label
         self.charge = charge
         self.occupancy = occupancy
